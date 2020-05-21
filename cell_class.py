0a5405fe57a68702741faa433f8cde7573d368ce
Abhinav <abhinavrastogi9596@gmail.com>
2020-05-21 09:28:01 +0530

    Initial Commit

---
+++ cell_class.py
@@ -0,0 +1,49 @@
+import pygame, random
+
+class cell:
+
+    def __init__(self, surface, grid_x, grid_y):
+
+        self.alive = False
+        self.surface = surface
+        self.grid_x= grid_x
+        self.grid_y= grid_y
+        self.image= pygame.Surface((20,20))
+        self.rect= self.image.get_rect()
+        self.neighbours =[]
+
+    def update(self):
+        self.rect.topleft = (self.grid_x*20, self.grid_y*20)
+
+
+    def draw(self):
+        if self.alive:
+            self.image.fill((0,0,0))
+        else:
+            self.image.fill((0,0,0))
+            pygame.draw.rect(self.image,(255,255,255) , (1 , 1 , 16,16))
+        self.surface.blit(self.image, (self.grid_x*20, self.grid_y*20))
+
+    def get_neighbours(self, grid):
+        neighbour_list = [[0,1], [1,1] , [1,0], [-1,1], [-1,0], [-1,-1] , [0,-1], [1,-1]]
+        for neighbour in neighbour_list:
+            neighbour[0]+=self.grid_x
+            neighbour[1]+=self.grid_y
+
+        for neighbour in neighbour_list:
+            if neighbour[0] < 0:
+                neighbour[0]+=30
+            if neighbour[1] < 0:
+                neighbour[1] +=30
+            if neighbour[1] > 29:
+                neighbour[1]-=30
+            if neighbour[0] > 30:
+                neighbour[0] -=30
+
+        for neighbour in neighbour_list:
+            try:
+                self.neighbours.append(grid[neighbour[1]][neighbour[0]])
+            except:
+                print(neighbour)
+
+
---
+++ gamewindow.py
@@ -0,0 +1,56 @@
+import pygame
+import sys
+from game_window_class import *
+WIDTH, HEIGHT = 800, 800
+BACKGROUND = (199,199,199)
+FPS=60
+
+def get_events():
+    global running
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+            if(mouse_on_grid(mouse_pos)):
+                click_cell(mouse_pos)
+
+
+def update():
+    game_window.update()
+
+
+def draw():
+    window.fill(BACKGROUND)
+    game_window.draw()
+
+def mouse_on_grid(pos):
+    if pos[0] > 100 and pos[0] < WIDTH-100:
+        if pos[1] > 180 and pos[1] < HEIGHT-20:
+            return True
+    return False;
+
+def click_cell(pos):
+    grid_pos = [pos[0]-100 , pos[1]-180]
+    grid_pos[0] = grid_pos[0]//20
+    grid_pos[1] = grid_pos[1] // 20
+    if game_window.grid[grid_pos[1]][grid_pos[0]].alive:
+        game_window.grid[grid_pos[1]][grid_pos[0]].alive = False
+    else:
+        game_window.grid[grid_pos[1]][grid_pos[0]].alive = True
+
+
+pygame.init()
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+clock=pygame.time.Clock()
+game_window=Game_window(window,100 , 180)
+
+running = True
+while running:
+    get_events()
+    update()
+    draw()
+    pygame.display.update()
+    clock.tick(FPS)
+pygame.quit()
+sys.exit()
